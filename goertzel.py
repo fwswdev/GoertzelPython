8eaeaed3737d1ce5a83cdc5be40ffab1d1f5840a
jjp <jjp>
2016-02-15 17:23:21 +0800

    cleaned the comments

--- goertzel.py
+++ goertzel.py
@@ -7,8 +7,7 @@ class Goertzel:
     Q1 = 0
     _N = 1000
 
-    #MAXN = 200
-    ADCCENTER = 512
+    ADCCENTER = 512 # used to remove the DC offset
 
     omega=0
     coeff=0
@@ -20,30 +19,20 @@ class Goertzel:
 
 
     def ChangeParameters(self , TARGET_FREQUENCY, N, SAMPLING_FREQUENCY):
-        self._SAMPLING_FREQUENCY=SAMPLING_FREQUENCY;	#on 16mhz, ~8928.57142857143, on 8mhz ~44444
-        self._TARGET_FREQUENCY=TARGET_FREQUENCY; #should be integer of SAMPLING_RATE/N
+        self._SAMPLING_FREQUENCY=SAMPLING_FREQUENCY*1.0;	#on 16mhz, ~8928.57142857143, on 8mhz ~44444
+        self._TARGET_FREQUENCY=TARGET_FREQUENCY*1.0; #should be integer of SAMPLING_RATE/N
         self._N=N;
-
-        k = int(0.5*0 + ((self._N * TARGET_FREQUENCY) / SAMPLING_FREQUENCY));
-        self.omega = (2.0 * math.pi * k) / self._N
-
-
-        #self.omega = (2.0 * math.pi * self._TARGET_FREQUENCY) / self._SAMPLING_FREQUENCY;
+        self.omega = (2.0 * math.pi * self._TARGET_FREQUENCY) / self._SAMPLING_FREQUENCY;
         self.coeff = 2.0 * math.cos(self.omega);
         self.ResetGoertzel();
+
+        print "Bin Width ", SAMPLING_FREQUENCY/(N*1.0)
 
 
     def ProcessSample(self,sample):
         Q0 = self.coeff * self.Q1 - self.Q2 + (sample - self.ADCCENTER)
         self.Q2 = self.Q1
         self.Q1 = Q0
-
-
-##    def Sample(self,sensorPin):
-##        self.testData = []
-##        for x in range(self._N):
-##            self.testData.append(0)
-
 
     def Detect(self):
         for index in range(self._N):
